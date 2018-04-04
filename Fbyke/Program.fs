fd82d67aef16108b9034bf7f8a8fbec47bbb3359
A. Prooks <aprooks@live.ru>
2018-04-04 23:06:21 +0200

    working sample

--- Fbyke/Program.fs
+++ Fbyke/Program.fs
@@ -1,5 +1,4 @@
-﻿
-open System
+﻿open System
 open System.Net
 open System.Reflection
 
@@ -13,12 +12,17 @@ open Orleankka.Cluster
 open Orleankka.Client
 open Orleankka.FSharp
 
+open Byke
+open Grains
+
+
 [<EntryPoint>]
 let main argv =    
    
     printfn "Running demo. Booting cluster might take some time ...\n"
 
     let sb = new SiloHostBuilder()
+    sb.AddAssembly(typeof<Byke.Message>.Assembly)
     sb.AddAssembly(Assembly.GetExecutingAssembly())
     sb.UseOrleankka() |> ignore
 
@@ -28,6 +32,14 @@ let main argv =
     let system = client.ActorSystem()
     
     printfn "Cluster booted!"
+
+    let initScript = task {
+      
+      let byke = ActorSystem.typedActorOf<IByke, Message>(system,"first")
+      do! byke <! Reserve "Alex"
+    }
+
+    initScript.Wait()
 
     Console.ReadKey() |> ignore 
     0
