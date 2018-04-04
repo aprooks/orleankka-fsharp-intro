91c222b7f10ef73fd11a1c6bcb381ef2a9db980d
A. Prooks <aprooks@live.ru>
2018-04-04 22:22:04 +0200

    initial

--- Fbyke/Program.fs
+++ Fbyke/Program.fs
@@ -1,8 +1,33 @@
-﻿// Learn more about F# at http://fsharp.org
-
+﻿
 open System
+open System.Net
+open System.Reflection
+
+open FSharp.Control.Tasks
+open Orleans
+open Orleans.Hosting
+open Orleans.Configuration
+open Orleans.Runtime
+open Orleankka
+open Orleankka.Cluster
+open Orleankka.Client
+open Orleankka.FSharp
 
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+let main argv =    
+   
+    printfn "Running demo. Booting cluster might take some time ...\n"
+
+    let sb = new SiloHostBuilder()
+    sb.AddAssembly(Assembly.GetExecutingAssembly())
+    sb.UseOrleankka() |> ignore
+
+    use host = sb.Start().Result
+    use client = host.Connect().Result
+
+    let system = client.ActorSystem()
+    
+    printfn "Cluster booted!"
+
+    Console.ReadKey() |> ignore 
+    0
